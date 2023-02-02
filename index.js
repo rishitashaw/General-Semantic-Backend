660afc94d25155a86cce7a00008ae6a7e3d1d7ef
Rishita Shaw <rishitashaw49@gmail.com>
2023-02-02 17:50:16 +0530

    backend added

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.js')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config;
+
+app.use(express.static(__dirname + '/static'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.htm');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => console.log('listening on port ' + PORT));
